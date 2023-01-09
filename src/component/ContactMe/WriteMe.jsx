e4083926eab616917d275c573d99b0945bc594c2
edmundchan70 <edmundchan70@gmail.com>
2023-01-09 17:28:17 -0500

    Fixed input area error

--- src/component/ContactMe/WriteMe.jsx
+++ src/component/ContactMe/WriteMe.jsx
@@ -9,10 +9,10 @@ function WriteMe() {
         <form className="writeMe_box container" action='https://formsubmit.co/edmundchan70@gmail.com' method='POST'>
 
             <label htmlFor="" className="contactMe__form-div">Name</label>
-            <input type="text" name="email" className="writeMe_input" placeholder='Type your Name ...' />
+            <input type="text" className="writeMe_input" placeholder='Type your Name ...' />
 
             <label htmlFor="" className="contactMe__form-div">Email</label>
-            <input type="email" className="writeMe_input" placeholder='Type your Email ...' />
+            <input type="email"  name="email" className="writeMe_input" placeholder='Type your Email ...' />
 
             <label htmlFor="" className="contactMe__form-div">Message</label>
             <textarea name="message" className='writeMe_input' id="" cols="30" rows="10" placeholder='Enter your message here '></textarea>
